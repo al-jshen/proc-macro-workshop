c9e3c5b58b124360a2e0a3b065956257dc5a5bec
Jeff Shen <jshen2014@hotmail.com>
2022-07-29 22:18:23 -0700

    ?????

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
+use quote::{format_ident, quote, ToTokens};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
@@ -12,6 +12,21 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let structname = input.ident;
     let structbuildername = format_ident!("{}Builder", structname);
+
+    match input.data {
+        syn::Data::Struct(s) => match s.fields {
+            syn::Fields::Named(f) => {
+                for i in f.named.iter() {
+                    eprintln!("{:#?}", i.ident.as_ref().unwrap());
+                    eprintln!("{:#?}", i.ty.to_owned().to_token_stream());
+                }
+            }
+            syn::Fields::Unnamed(_) => todo!(),
+            syn::Fields::Unit => todo!(),
+        },
+        syn::Data::Enum(_) => todo!(),
+        syn::Data::Union(_) => todo!(),
+    }
 
     let output_tokens = quote! {
         pub struct #structbuildername {
@@ -31,6 +46,14 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 }
             }
         }
+
+        impl #structbuildername {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+         }
+
     };
 
     TokenStream::from(output_tokens)
